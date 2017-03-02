362f98f9a863ffe1c4f541a6d23703a3fc56a170
maduar <maduar86@gmail.com>
2017-03-02 17:01:02 +0800

    1.  update zhihucron

--- routes/connect/http_log.js
+++ routes/connect/http_log.js
@@ -3,52 +3,55 @@
  */
 
 const uuid = require('uuid'),
-    moment = require('moment');
+    moment = require('moment'),
+    myUtil = require('../util/myUtil'),
+    Promise = require('bluebird');
 
 module.exports = {
     httpLog: function (  ) {
         return function ( req, res, next ) {
 
 
-            const reg = /:|f/g;
-
-            const ip = req.connection.remoteAddress
+            const reg = /:|f/g,
+                ip = req.connection.remoteAddress
                         ? req.connection.remoteAddress.replace(reg, '')
-                        : ( req.ip ? req.ip.replace(reg, '') : 0);
-
-            const data = {
-                ip: ip,
-                log_uuid: uuid.v1().replace(/[-]/g, ''),
-                pid: process.pid,
-                url: req.url,
-                method: req.method,
-                timestamp: Date.parse(new Date()),
-                create_time: moment().format('YYYY-MM-DD HH:mm:ss'),
-                parameters: (req.method === 'POST') ? req.body : req.query
-            };
-
-            const parameters_sql = JSON.stringify(data.parameters);
-
-            const sqlStr = `INSERT INTO secl_api VALUES
-                            (
-                                '${data.log_uuid}', '${data.ip}', '${data.url}', '${data.method}', 
-                                '${parameters_sql}', '${data.timestamp}', '', '${config.app_id}', 'ACTIVE',
-                                '${data.create_time}'
-                              
-                            )`;
-
-            sequelize.query(sqlStr)
-                .then(() => {
-                    LogFile.info('api记录成功');
-                })
-                .catch(err => {
-                    LogFile.info('log_api is wrong.', err);
-                });
-
+                        : ( req.ip ? req.ip.replace(reg, '') : 0),
+                data = {
+                    ip: ip,
+                    log_uuid: uuid.v1().replace(/[-]/g, ''),
+                    pid: process.pid,
+                    url: req.url,
+                    method: req.method,
+                    timestamp: Date.parse(new Date()),
+                    create_time: moment().format('YYYY-MM-DD HH:mm:ss'),
+                    parameters: (req.method === 'POST') ? req.body : req.query
+                },
+                parameters_sql = JSON.stringify(data.parameters),
+                sqlStr = `INSERT INTO secl_api VALUES('${data.log_uuid}', '${data.ip}', '${data.url}', '${data.method}', 
+                             '${parameters_sql}', '${data.timestamp}', '', '${config.app_id}', 'ACTIVE','${data.create_time}')`,
+                exec_data = {
+                    user_id: '',
+                    api: data.url,
+                    client_host: data.ip,
+                    exec_sql: JSON.stringify(sqlStr),
+                    exec_duration: 0
+                };
 
             LogFile.info(JSON.stringify(data));
 
-            next();
+            return Promise.all([
+                sequelize.query(sqlStr),
+                myUtil.recodeExecSql(exec_data)
+            ])
+            .spread((data, result) => {
+                next();
+                LogFile.info('api记录成功');
+            })
+            .catch(err => {
+                next();
+                LogFile.info('log_api is wrong.', err);
+            });
+
         }
 
     }
--- routes/util/myUtil.js
+++ routes/util/myUtil.js
@@ -1,6 +1,10 @@
 /**
  * Created by maduar on 10/02/2017.
  */
+
+const uuid = require('uuid');
+
+
 //验证单个邮箱地址
 function verifyEmail(email) {
     var reg = /^[a-z_0-9.-]{1,64}@([a-z0-9-]{1,200}.){1,5}[a-z]{1,6}$/;
@@ -26,6 +30,43 @@ module.exports = {
             }
         })
         return _result;
+    },
+
+    recodeExecSql: function (data) {
+
+        const parameters = {
+            id: uuid.v1().replace(/[-]/g, ''),
+            user_id: data.user_id || '',
+            exec_time: Date.parse(new Date()),
+            exec_api: data.api,
+            client_host: data.client_host,
+            exec_sql: '', //data.exec_sql,
+            exec_duration: data.duration ?  Number(data.duration) : 0,
+            duration_unit: '0',
+            exec_result: 'SUCCESS'
+        }
+
+        const sqlStr = `INSERT INTO secl_sql VALUES(
+                        '${parameters.id}', '${parameters.user_id}', '${parameters.exec_time}', '${parameters.exec_api}', 
+                        '${parameters.client_host}', '${parameters.exec_sql}', ${parameters.exec_duration} , '${parameters.duration_unit}', 
+                        '${parameters.exec_result}')`;
+
+        return sequelize.query(sqlStr)
+            .then((data) => {
+                console.log();
+            })
+            .catch(err => {
+                LogFile.info('log_api is wrong.', err);
+            });
+    },
+
+    getUuid: function () {
+        return uuid.v1().replace(/[-]/g, '');
+    },
+
+    getTimestamp: function () {
+        return Date.parse(new Date());
     }
-}
-module.exports.verifyEmail = verifyEmail;
+};
+
+module.exports.verifyEmail = verifyEmail
